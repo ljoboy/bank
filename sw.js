99a842c90fbb0c5327ca5855ff19f7fbd62702b7
PietrzakAdrian <contact.pietrzakadrian@gmail.com>
2018-11-29 21:51:33 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ var __wpo = {
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "2018-11-29 21:49:42",
+  "version": "2018-11-29 21:51:32",
   "name": "webpack-offline",
   "pluginVersion": "5.0.5",
   "relativePaths": false
